dcc07854d5d199ec5bb721ad7056302e8368cbfe
xDunyay <dunya_waldun@hotmail.com>
2018-11-13 21:35:37 +0100

    Die ersten Schritte wurden getan.

---
+++ src/GameOfLife.java
@@ -0,0 +1,52 @@
+/**
+ * @author Ajmal
+ * @since 13.11.18
+ * @version 1.9
+ * */
+
+public class GameOfLife implements IGameOfLife {
+
+    {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void showGrid() {
+
+    }
+
+    @Override
+    public void setAlive(int x, int y) {
+
+    }
+
+    @Override
+    public void setDead(int x, int y) {
+
+    }
+
+    @Override
+    public int getLiveNeighbors(int x, int y) {
+        return 0;
+    }
+
+    @Override
+    public void runGeneration() {
+
+    }
+
+    @Override
+    public void runGenerations(int howMany_) {
+
+    }
+
+    @Override
+    public int[][] getGrid() {
+        return new int[0][];
+    }
+}
